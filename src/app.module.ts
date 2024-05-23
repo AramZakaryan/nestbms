71f681eda342656cb605f4c4f815eac06cafdf14
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-05-23 22:18:31 +0400

    add config, env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,20 +6,28 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Author } from './author/entities/author.entity'
 import { BookModule } from './book/book.module'
 import { Book } from './book/entities/book.entity'
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module'
+import { UserModule } from './user/user.module'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '1234567a@',
-      database: 'bms',
-      entities: [Author, Book],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
     }),
     AuthorModule,
     BookModule,
